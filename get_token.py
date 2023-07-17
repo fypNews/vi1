c021667a15cf1f565739fe146e7408c3310f0095
fypNews <124069362+fypNews@users.noreply.github.com>
2023-07-18 00:33:00 +0700

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -15,4 +15,9 @@ response = requests.post(tokke, headers=headers)
 result = response.json()
 token = result.get('token', '')
 
+# Menyimpan hasil token ke file live/204.json
+output_file = 'live/204.json'
+with open(output_file, 'w') as file:
+    json.dump(result, file)
+
 print(token)
