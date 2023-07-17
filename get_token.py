6e52542f8a592bc1284dad1354dd3b26705f8180
fypNews <124069362+fypNews@users.noreply.github.com>
2023-07-18 00:36:13 +0700

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -15,8 +15,13 @@ response = requests.post(tokke, headers=headers)
 result = response.json()
 token = result.get('token', '')
 
+# Membuat direktori 'live' jika belum ada
+live_dir = 'live'
+if not os.path.exists(live_dir):
+    os.makedirs(live_dir)
+
 # Menyimpan hasil token ke file live/204.json
-output_file = 'live/204.json'
+output_file = os.path.join(live_dir, '204.json')
 with open(output_file, 'w') as file:
     json.dump(result, file)
 
